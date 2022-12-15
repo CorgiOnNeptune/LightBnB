d56fc98826f2113a20ef3823caacb65590ebca6b
Justin <justinjdbennett@gmail.com>
2022-12-15 08:12:02 +0000

    Fixed getAllProperties function

--- LightBnB_WebApp/server/database.js
+++ LightBnB_WebApp/server/database.js
@@ -79,12 +79,11 @@ exports.getAllReservations = getAllReservations;
  * @return {Promise<[{}]>}  A promise to the properties.
  */
 const getAllProperties = (options, limit = 10) => {
-  return pool;
-  
-  pool.query(`SELECT * FROM properties LIMIT $1`, [limit])
-    .then((res) => {
-      console.log(res.rows);
-      return res.rows;
+  return pool
+    .query(`SELECT * FROM properties LIMIT $1`, [limit])
+    .then((result) => {
+      console.log(result.rows);
+      return result.rows;
     })
     .catch((err) => {
       console.log(err.message);
