4ec481ce9d595fc383a5c3925d4e4d32640d7dda
Justin <justinjdbennett@gmail.com>
2022-12-16 02:59:07 +0000

    Add properties to the database

--- LightBnB_WebApp/server/database.js
+++ LightBnB_WebApp/server/database.js
@@ -217,10 +217,25 @@ exports.getAllProperties = getAllProperties;
  * @param {{}} property An object containing all of the property details.
  * @return {Promise<{}>} A promise to the property.
  */
-const addProperty = function(property) {
-  const propertyId = Object.keys(properties).length + 1;
-  property.id = propertyId;
-  properties[propertyId] = property;
-  return Promise.resolve(property);
-}
+const addProperty = property => {
+  const propertyValues = ['owner_id', 'title', 'description', 'thumbnail_photo_url', 'cover_photo_url', 'cost_per_night', 'street', 'city', 'province', 'post_code', 'country', 'parking_spaces', 'number_of_bathrooms', 'number_of_bedrooms'];
+
+  const queryString = `
+  INSERT INTO properties(${propertyValues.join(', ')})
+  VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14)
+  RETURNING *;
+  `;
+
+  const values = propertyValues.map((value) => property[value]);
+
+  return pool
+    .query(queryString, values)
+    .then((res) => {
+      return res.rows[0];
+    })
+    .catch((err) => {
+      console.log(err.message);
+      return null;
+    });
+};
 exports.addProperty = addProperty;
